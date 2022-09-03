9a3fd6148d58dd6e9c76fd87e320b1ff8b43667c
dot-1x <mugiwaraluffy2003@gmail.com>
2022-09-03 23:52:50 +0700

    zzzz

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ with open("_debugs/config.json", "r") as cfg:
 
 if __name__ == "__main__":
     db = connection(CONFIG["database"])
-    bot = Bot(CONFIG, token=CONFIG["devtoken"], db=db, debug=True)
+    bot = Bot(CONFIG, token=CONFIG["token"], db=db, debug=False)
     bot.run()
